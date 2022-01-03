af97b04238d19f3c093462fc75b0c3c812f7ba2c
Michael Monihan <mrmonihan@hotmail.com>
2022-01-02 23:31:12 -0500

    Added full API support as well as working 5 day forecast

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Weather Dashboard
 
-
+In this website I have created a dashboard where a user can find weather information for a specified place. The website works using OpenWeather API that allows the user to find out the temperature, wind speed, humidity, UV index, as well as a corresponding icon. The users are also able to see weather data for up to five days in advance. 
 
 # Website features 
 
-![Website Features Image]()
+![Website Features Image](assets/webFeat.PNG)
 
-[Live website link](https://mikemonihan.github.io/Code_Quiz/)
+[Live website link](https://mikemonihan.github.io/Weather_Dashboard/)
 
-[Live github repo link](https://github.com/MikeMonihan/Code_Quiz)
+[Live github repo link](https://github.com/MikeMonihan/Weather_Dashboard)
--- assets/script.js
+++ assets/script.js
@@ -5,9 +5,7 @@ var weatherToday = document.querySelector("#weatherToday");
 var fiveDay = document.querySelector("#fiveDay");
 var searchCity = document.querySelector('#searchCity');
 var searchCityA = document.querySelector('#searchCityAgain');
-
 var tOD = document.querySelector('#tOD');
-
 var temp = document.querySelector("#temp");
 var wind = document.querySelector("#wind");
 var humid = document.querySelector("#humid");
@@ -77,7 +75,13 @@ var getLocation = function(city) {
          console.log(response);
          response.json().then(function(data) {
            console.log(data);    
-           displayWeather(data);    
+           displayWeather(data);
+           displayFiveDay1(data);   
+           displayFiveDay2(data);
+           displayFiveDay3(data);
+           displayFiveDay4(data);
+           displayFiveDay5(data);
+           setIcon(data);
          });
        } else {
          alert("Error: " + response.statusText);
@@ -94,6 +98,8 @@ var getLocation = function(city) {
   }; 
 
   function displayWeather(city){
+    
+    document.querySelector("#icon").src = "http://openweathermap.org/img/wn/"+ city.current.weather[0].icon +"@2x.png";
 
     temp.textContent = "Temp: " + city.current.temp + " ℉";
 
@@ -110,8 +116,106 @@ var getLocation = function(city) {
     humanDateFormat = dateObject.getUTCDate() 
 
     tOD.textContent = "(" + dateObject.getUTCMonth() + "/" + dateObject.getDate() + "/" + dateObject.getFullYear() + ")";
+    
   }
 
+  function displayFiveDay1(city){
+    
+    
+    document.querySelector('#temp1').textContent = "Temp: " + city.daily[1].temp.day + " ℉";
+
+    document.querySelector('#wind1').textContent = "Wind: " + city.daily[1].wind_speed + " MPH";
+
+    document.querySelector('#humid1').textContent = "Humidity: " + city.daily[1].humidity + " %";
+
+    milliseconds = city.daily[1].dt * 1000
+
+    dateObject = new Date(milliseconds)
+
+    humanDateFormat = dateObject.getUTCDate() 
+
+    head1.textContent = "(" + dateObject.getUTCMonth() + "/" + dateObject.getDate() + "/" + dateObject.getFullYear() + ")";
+  }
+  function displayFiveDay2(city){
+    
+    document.querySelector("#icon1").src = "http://openweathermap.org/img/wn/"+ city.daily[2].icon +"@2x.png"
+
+    document.querySelector('#temp2').textContent = "Temp: " + city.daily[2].temp.day + " ℉";
+
+    document.querySelector('#wind2').textContent = "Wind: " + city.daily[2].wind_speed + " MPH";
+
+    document.querySelector('#humid2').textContent = "Humidity: " + city.daily[2].humidity + " %";
+
+    milliseconds = city.daily[2].dt * 1000
+
+    dateObject = new Date(milliseconds)
+
+    humanDateFormat = dateObject.getUTCDate() 
+
+    head2.textContent = "(" + dateObject.getUTCMonth() + "/" + dateObject.getDate() + "/" + dateObject.getFullYear() + ")";
+  }
+  function displayFiveDay3(city){
+    
+    document.querySelector('#temp3').textContent = "Temp: " + city.daily[3].temp.day + " ℉";
+
+    document.querySelector('#wind3').textContent = "Wind: " + city.daily[3].wind_speed + " MPH";
+
+    document.querySelector('#humid3').textContent = "Humidity: " + city.daily[3].humidity + " %";
+
+    milliseconds = city.daily[3].dt * 1000
+
+    dateObject = new Date(milliseconds)
+
+    humanDateFormat = dateObject.getUTCDate() 
+
+    head3.textContent = "(" + dateObject.getUTCMonth() + "/" + dateObject.getDate() + "/" + dateObject.getFullYear() + ")";
+  }
+  function displayFiveDay4(city){
+    
+    document.querySelector('#temp4').textContent = "Temp: " + city.daily[4].temp.day + " ℉";
+
+    document.querySelector('#wind4').textContent = "Wind: " + city.daily[4].wind_speed + " MPH";
+
+    document.querySelector('#humid4').textContent = "Humidity: " + city.daily[4].humidity + " %";
+
+    milliseconds = city.daily[4].dt * 1000
+
+    dateObject = new Date(milliseconds)
+
+    humanDateFormat = dateObject.getUTCDate() 
+
+    head4.textContent = "(" + dateObject.getUTCMonth() + "/" + dateObject.getDate() + "/" + dateObject.getFullYear() + ")";
+  }
+  function displayFiveDay5(city){
+    
+    document.querySelector('#temp5').textContent = "Temp: " + city.daily[5].temp.day + " ℉";
+
+    document.querySelector('#wind5').textContent = "Wind: " + city.daily[5].wind_speed + " MPH";
+
+    document.querySelector('#humid5').textContent = "Humidity: " + city.daily[5].humidity + " %";
+
+    milliseconds = city.daily[5].dt * 1000
+
+    dateObject = new Date(milliseconds)
+
+    humanDateFormat = dateObject.getUTCDate() 
+
+    head5.textContent = "(" + dateObject.getUTCMonth() + "/" + dateObject.getDate() + "/" + dateObject.getFullYear() + ")";
+  }
+
+  function setIcon(city){
+
+    document.querySelector("#icon1").src = "http://openweathermap.org/img/wn/"+ city.daily[1].weather[0].icon +"@2x.png";
+
+    document.querySelector("#icon2").src = "http://openweathermap.org/img/wn/"+ city.daily[2].weather[0].icon +"@2x.png";
+
+    document.querySelector("#icon3").src = "http://openweathermap.org/img/wn/"+ city.daily[3].weather[0].icon +"@2x.png";
+
+    document.querySelector("#icon4").src = "http://openweathermap.org/img/wn/"+ city.daily[4].weather[0].icon +"@2x.png";
+
+    document.querySelector("#icon5").src = "http://openweathermap.org/img/wn/"+ city.daily[5].weather[0].icon +"@2x.png";
+
+  }
 
  cityForm.addEventListener("submit", formSubmitHandler);
 
